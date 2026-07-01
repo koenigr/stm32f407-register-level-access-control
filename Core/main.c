71f4659d8bb24f51167899f113d8fb0757234770
Regina König <10077142+koenigr@users.noreply.github.com>
2026-07-01 18:35:29 +0200

    Add CubeIDE code

--- Core/main.c
+++ Core/main.c
@@ -4,10 +4,24 @@
 
 int main(void)
 {
+	RCC_AHB1ENR_t volatile 	*const pClkCtrlReg   = ADDR_REG_AHB1ENR;
+	GPIOx_MODE_t  volatile 	*const pPortDModeReg = ADDR_REG_GPIOD_MODE;
+	GPIOx_ODR_t   volatile  *const pPortDOutReg  = ADDR_REG_GPIOD_OD;
 
-	volatile uint32_t moder = GPIOA->MODER;
+	//1. enable the clock for GPIOD peripheral in the AHB1ENR
+	pClkCtrlReg->gpiod_en = CLOCK_ENABLE; // for GPIO D
 
-	GPIO_RegDef_t *gpio = GPIOA;
+	// Configure the mode of the PD12 as output
+	// a. clear the 24th and 25th bit positions (CLEAR)
+	pPortDModeReg->mode_pin12 = MODE_CONF_OUTPUT;
 
-	return 0;
+	while(1) {
+		pPortDOutReg->od_pin12 = PIN_STATE_HIGH;
+
+		for(uint32_t i = 0; i < DELAY_COUNT; i++);
+
+		pPortDOutReg->od_pin12 = PIN_STATE_LOW;
+
+		for(uint32_t i = 0; i < DELAY_COUNT; i++);
+	}
 }
