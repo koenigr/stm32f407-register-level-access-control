a1931d179eb08826dc47a38a499b0b40f6c62c89
Regina König <10077142+koenigr@users.noreply.github.com>
2026-07-07 21:36:49 +0200

    Add image with keypad connection

--- Core/main.c
+++ Core/main.c
@@ -1,34 +1,23 @@
 #include <stdint.h>
 #include "stm32f407xx.h"
 #include "stm32f407xx_gpio.h"
+#include "keypad.h"
 
-int main(void)
-{
+
+int main(void) {
 
 	GPIO_PeriClockControl(GPIOD, ENABLE);
 
-	GPIO_InitOutput(GPIOD, 12);
+	GPIO_InitOutput(GPIOD, 12); // LED
+
+	Keypad_Init();
 
 	while(1) {
+		char key = Keypad_Scan();
 
-		GPIO_TogglePin(GPIOD, 12);
-		for(uint32_t i = 0; i < DELAY_COUNT; i++);
-
+		if(key) {
+			volatile char pressed_key = key;
+			GPIO_TogglePin(GPIOD, 12);
+		}
 	}
 }
-
-/*
-int main(void)
-{_init(GPIOD, )
-    Keypad_Init();
-
-    while(1)
-    {
-        char key = Keypad_Scan();
-
-        if(key)
-        {
-            // Debug
-        }
-    }
-}*/
